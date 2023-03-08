3c5304a774b5b8cbd058a76b08340bc5cfe833bf
Sita Ram Thing <sitaramthing667@gmail.com>
2023-03-09 00:57:06 +0545

    change so some design

---
+++ NestedRecyclerView/app/src/main/java/com/example/nestedrecyclerview/Model/ChildAppsModel.java
@@ -0,0 +1,14 @@
+package com.example.nestedrecyclerview.Model;
+
+public class ChildAppsModel {
+    public int image;
+    public String name;
+    public String star;
+
+    // constructor
+    public ChildAppsModel(int image, String name, String star) {
+        this.image = image;
+        this.name = name;
+        this.star = star;
+    }
+}
---
+++ NestedRecyclerView/app/src/main/java/com/example/nestedrecyclerview/Model/ParentAppsModel.java
@@ -0,0 +1,12 @@
+package com.example.nestedrecyclerview.Model;
+
+import java.util.List;
+
+public class ParentAppsModel {
+    public String title;
+    public List<ChildAppsModel> childModelClassList;
+    public ParentAppsModel(String title, List<ChildAppsModel> childModelClassList) {
+        this.title = title;
+        this.childModelClassList = childModelClassList;
+    }
+}
